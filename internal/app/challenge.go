8cccac49490e1dc6a87004e54279573e23ba751a
Flávio Rodolfo <rodolphoflavio@gmail.com>
2022-05-16 01:53:26 -0300

    Add method to calculate cart values per consumer

--- internal/app/challenge.go
+++ internal/app/challenge.go
@@ -1,1 +1,38 @@
 package app
+
+import (
+	"stone-challenge/internal/model"
+)
+
+func calculateCartValues(list []model.ShoppingCart) int {
+	total := 0
+	for _, cart := range list {
+		total += cart.Item.Price * cart.Quantity
+	}
+	return total
+}
+
+func divideTotalValue(totalValue int, totalPersons int) []int {
+
+	personValue := totalValue / totalPersons
+	remainder := totalValue % totalPersons
+	var valuesList []int
+	for i := 0; i < totalPersons; i++ {
+		valuesList = append(valuesList, personValue)
+		if remainder > 0 {
+			valuesList[i]++
+			remainder--
+		}
+	}
+	return valuesList
+}
+
+func CalculateValues(cart []model.ShoppingCart, customer []model.Customer) map[string]int {
+	totalValue := calculateCartValues(cart)
+	valuesList := divideTotalValue(totalValue, len(customer))
+	resultMap := make(map[string]int)
+	for index, value := range valuesList {
+		resultMap[customer[index].Email] = value
+	}
+	return resultMap
+}
